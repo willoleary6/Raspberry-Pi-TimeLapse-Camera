ebf2c0f371e9a2dd26e3f2d2903c8d4eeb10121c
willoleary6 <willoleary6@gmail.com>
2021-02-22 15:35:35 +0000

    Good to go

--- timelapse_capture.py
+++ timelapse_capture.py
@@ -1,31 +1,31 @@
 from time import sleep
 from picamera import PiCamera
 import datetime
+import config as Config
+import S3Handler
 # config
-camera_resolution = (2592, 1944)
-camera_rotation= 180
-datetime_format = "%Y_%m_%d_%H_%M_%S"
-seconds_to_sleep_until_next_photo = 10
-seconds_of_exposure = 2
-photo_directory = '/home/pi/Pictures/Timelapse/'
-file_extension = '.png'
 
 
-camera = PiCamera(resolution=camera_resolution)
-
+camera = PiCamera(resolution=Config.camera_resolution)
+camera.annotate_text_size = 50 # (values 6 to 160, default is 32)
 # rotate camera orientation by 180 degrees
-camera.rotation = camera_rotation
+camera.rotation = Config.camera_rotation
+
 while(True):
     # lets get the datetime format right
     raw_datetime = datetime.datetime.now()
-    formated_datetime = raw_datetime.strftime(datetime_format)
-    # Explicitly open a new file called my_image.jpg
-    my_file = open(photo_directory+formated_datetime+file_extension, 'wb')
-    camera.start_preview()
-    sleep(seconds_of_exposure)
-
-    camera.capture(my_file)
-    # At this point my_file.flush() has been called, but the file has
-    # not yet been closed
-    my_file.close()
-    sleep(seconds_to_sleep_until_next_photo)
+    # dont need to run this all day
+    if raw_datetime.hour >= Config.start_time and raw_datetime.hour < Config.end_time:
+        formated_datetime = raw_datetime.strftime(Config.datetime_format)
+        # Explicitly open a new file
+        filename = Config.photo_directory+formated_datetime+Config.file_extension
+        my_file = open(filename, 'wb')
+        camera.start_preview()
+        sleep(Config.seconds_of_exposure)
+        camera.annotate_text = formated_datetime
+        camera.capture(my_file)
+        # At this point my_file.flush() has been called, but the file has
+        # not yet been closed
+        my_file.close()
+        S3Handler.upload_file(filename,'timelapse-dump', filename.rsplit('/', 1)[-1])
+        sleep(Config.seconds_to_sleep_until_next_photo)
