9a0d080cf1d9ffd216551cdf84e1dd6fb73ca75b
willoleary6 <willoleary6@gmail.com>
2021-02-23 09:46:19 +0000

    file format didnt comply with windows, woops...

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # timelapse capture
 camera_resolution = (2592, 1944)
 camera_rotation= 180
-datetime_format = "%Y-%m-%d %H:%M:%S"
+datetime_format = "%Y-%m-%d %H_%M_%S"
 seconds_to_sleep_until_next_photo = 5
 seconds_of_exposure = 2
 photo_directory = '/home/pi/Pictures/Timelapse/'
